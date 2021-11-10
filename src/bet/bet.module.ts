a69a6a5ed6dd802f3786da91c4f3f6edfd95175e
Christian Fleury <chfleurysiq@gmail.com>
2021-11-10 17:20:55 -0300

    refactor: remove unnecessary imports

--- src/bet/bet.module.ts
+++ src/bet/bet.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Game } from 'src/game/game.entity';
-import { GameResolver } from 'src/game/game.resolver';
-import { GameService } from 'src/game/game.service';
 import { Bet } from './bet.entity';
 import { BetResolver } from './bet.resolver';
 import { BetService } from './bet.service';
@@ -10,6 +8,6 @@ import { BetService } from './bet.service';
 @Module({
   imports: [TypeOrmModule.forFeature([Bet, Game])],
 
-  providers: [BetResolver, BetService, GameService, GameResolver],
+  providers: [BetResolver, BetService],
 })
 export class BetModule {}
